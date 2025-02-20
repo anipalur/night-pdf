b55007c715fadc6b9e52ed8fae66d619708c3ff8
AP <135231141+anipalur@users.noreply.github.com>
2025-02-20 13:29:59 +0800

    Create background.js to detect when the extension icon is clicked

---
+++ background.js
@@ -0,0 +1,7 @@
+chrome.action.onClicked.addListener(tab => {
+    chrome.tabs.sendMessage(tab.id, { action: 'iconClicked' });
+	chrome.action.setBadgeText({ text: '🗸', tabId: tab.id });
+	setTimeout(() => {
+        chrome.action.setBadgeText({ text: '', tabId: tab.id });
+    }, 1_000);
+});
