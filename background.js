39ebdf380389532cbf4fb5c845397065a4eec26f
AP <135231141+anipalur@users.noreply.github.com>
2025-02-20 18:52:39 +0800

    Toggle extension based on type of active tab

--- background.js
+++ background.js
@@ -1,7 +1,42 @@
 chrome.action.onClicked.addListener(tab => {
-	chrome.tabs.sendMessage(tab.id, { action: 'iconClicked' });
-	chrome.action.setBadgeText({ text: '🗸', tabId: tab.id });
-	setTimeout(() => {
-		chrome.action.setBadgeText({ text: '', tabId: tab.id });
-	}, 1_000);
+	const isPdf = tab.url?.endsWith('.pdf');
+	if (isPdf) {
+		chrome.tabs.sendMessage(tab.id, { action: 'iconClicked' });
+		chrome.action.setBadgeText({ text: '🗸', tabId: tab.id });
+		chrome.action.setBadgeBackgroundColor({ color: '#00FF00' });
+		setTimeout(() => {
+			chrome.action.setBadgeText({ text: '', tabId: tab.id });
+			chrome.action.setBadgeTextColor({ color: null });
+		}, 500);
+	}
+});
+
+function toggleExtension(tab, tabId) {
+	const isPdf = tab.url.endsWith('.pdf');
+	if (isPdf) {
+		chrome.tabs.sendMessage(tabId, { action: 'enableDarkTheme' });
+		chrome.action.setBadgeText({ text: '', tabId: tabId });
+		chrome.action.setBadgeTextColor({ color: null });
+		chrome.action.setTitle({ title: 'Click to toggle dark mode!' })
+	} else {
+		chrome.tabs.sendMessage(tabId, { action: 'disableDarkTheme' });
+		chrome.action.setBadgeText({ text: '✗', tabId: tabId });
+		chrome.action.setBadgeBackgroundColor({ color: '#FF0000' });
+		chrome.action.setTitle({ title: 'Night PDF is unavailable on this page.' })
+	}
+}
+
+chrome.tabs.onActivated.addListener(activeInfo => {
+	currentTabId = activeInfo.tabId;
+	chrome.tabs.get(currentTabId, tab => {
+		if (tab && tab.url) {
+			toggleExtension(tab, currentTabId);
+		}
+	});
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+	if (changeInfo.status === 'complete' && tab.url) {
+		toggleExtension(tab, tabId);
+	}
 });
--- content-script.js
+++ content-script.js
@@ -12,10 +12,15 @@ let currentBackgroundColour = 0;
 const darkPdfDiv = document.createElement('div');
 darkPdfDiv.style.cssText = darkPdfDivStyles + `background-color: ${backgroundColours[currentBackgroundColour]};`;
 document.body.appendChild(darkPdfDiv);
+darkPdfDiv.style.display = 'none';
 
 chrome.runtime.onMessage.addListener(request => {
 	if (request.action === 'iconClicked') {
 		currentBackgroundColour = (currentBackgroundColour + 1) % backgroundColours.length;
 		darkPdfDiv.style.backgroundColor = backgroundColours[currentBackgroundColour];
+	} else if (request.action === 'enableDarkTheme') {
+		darkPdfDiv.style.display = 'block';
+	} else if (request.action === 'disableDarkTheme') {
+		darkPdfDiv.style.display = 'none';
 	}
 });
