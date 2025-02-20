d0e7adb9b5da7a9eadd12e7429bc4c845232406f
AP <135231141+anipalur@users.noreply.github.com>
2025-02-20 13:29:31 +0800

    Create content-script.js to apply dark theme to PDFs

---
+++ content-script.js
@@ -0,0 +1,21 @@
+const darkPdfDivStyles = `
+    pointer-events: none;
+    width: 100vw;
+    height: 100vh;
+    mix-blend-mode: difference;
+    z-index: 1;
+`;
+
+const backgroundColours = ['#e6e6e6', '#cccccc', 'transparent'];
+let currentBackgroundColour = 0;
+
+const darkPdfDiv = document.createElement('div');
+darkPdfDiv.style.cssText = darkPdfDivStyles + `background-color: ${backgroundColours[currentBackgroundColour]};`;
+document.body.appendChild(darkPdfDiv);
+
+chrome.runtime.onMessage.addListener(request => {
+    if (request.action === 'iconClicked') {
+        currentBackgroundColour = (currentBackgroundColour + 1) % backgroundColours.length;
+        darkPdfDiv.style.backgroundColor = backgroundColours[currentBackgroundColour];
+    }
+});
